176096ceb491fd689745eaba0d190ab0ae06b080
gfmcarey <gfmcarey@gmail.com>
2024-01-16 10:37:01 -0500

    Inline Handler in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -15,25 +15,26 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
   
   //uses stored value, if a value exists, to set initial state of the searchTerm
   /*
@@ -54,6 +55,16 @@ const App = () => {
   }, [searchTerm]);
   */
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React')
+
+  const [stories, setStories] = React.useState(initialStories)
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -80,7 +91,7 @@ const App = () => {
 
       <hr />
 
-      <List list = {searchedStories} />
+      <List list = {searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
@@ -132,15 +143,17 @@ const InputWithLabel = ({
   );
 };
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
     ))}
   </ul>
 );
 
-const Item = ({ item }) => (
+//In contrast, the second and more popular solution is to use an inline arrow function, 
+//which allows us to sneak in arguments like the item:
+const Item = ({ item, onRemoveItem }) => (
   <li>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -148,6 +161,11 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </li>
 );
 
